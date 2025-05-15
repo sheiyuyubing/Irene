a7860f3d1824037d8bd0fac1396c8496f9506061
syybing <3181644734@qq.com>
2025-05-15 21:22:41 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,9 +1,5 @@
-# open 'all.txt' and read each line as sgfFile
-# if sgfFile doesn't have 'HA[', append to allValidFile
-
 import os
 
-# find all sgf files in games/
 def findSgfFiles(path):
     sgfFiles = []
     for root, dirs, files in os.walk(path):
@@ -12,30 +8,32 @@ def findSgfFiles(path):
                 sgfFiles.append(os.path.join(root, file))
     return sgfFiles
 
+def read_file_safely(filepath):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            return f.read()
+    except UnicodeDecodeError:
+        with open(filepath, 'r', encoding='latin1') as f:
+            return f.read()
 
 allValidFile = []
-
 count = 0
 
 allSgfFiles = findSgfFiles('games')
 for sgfFile in allSgfFiles:
     try:
-        with open(sgfFile, 'r') as sgf:
-            data = sgf.read()
-        # DT[2017-08-24] date > 2000
+        data = read_file_safely(sgfFile)
         if 'HA[' not in data and 'DT[20' in data:
             allValidFile.append(sgfFile)
-    except:
-        print('Error: ' + sgfFile)
+    except Exception as e:
+        print('Error reading:', sgfFile, e)
 
     count += 1
     if count % 10000 == 0:
         print('Processed ' + str(count) + ' files')
 
-# write allValidFile to 'allValid.txt'
-with open('games/allValid2.txt', 'w') as allValid:
+with open('allValid2.txt', 'w', encoding='utf-8') as allValid:
     for sgfFile in allValidFile:
-        allValid.write(sgfFile)
-        allValid.write('\n')
+        allValid.write(sgfFile + '\n')
 
 print('Total:', len(allValidFile))
