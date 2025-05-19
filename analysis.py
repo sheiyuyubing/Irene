9f15ca612211ff4a774d5e3601eff990b9e8fbc1
syybing <3181644734@qq.com>
2025-05-19 23:33:27 +0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -8,9 +8,9 @@ analyze_thread = None
 
 import torch
 
-def get_top_moves(go,policy_dir,value_dir, topk=5):
-    policy = getPolicyNetResult(go,policy_dir)  # shape: (361,)
-    value = getValueNetResult(go,value_dir)    # float 0.0~1.0
+def get_top_moves(go,policy_model,value_model, topk=5):
+    policy = getPolicyNetResult(go,policy_model)  # shape: (361,)
+    value = getValueNetResult(go,value_model)    # float 0.0~1.0
     winrate = int(value * 100)  # 转换为 GTP 需要的整数格式
 
     # 获取预测中前 topk 个动作的位置索引（降序）
@@ -39,7 +39,7 @@ def get_top_moves(go,policy_dir,value_dir, topk=5):
 
 
 
-def start_analysis(go, interval,policy_dir,value_dir):
+def start_analysis(go, interval,policy_model,value_model):
     global analyze_thread, stop_event
 
     stop_event.clear()
@@ -48,7 +48,7 @@ def start_analysis(go, interval,policy_dir,value_dir):
 
         while not stop_event.is_set():
             # 这里模拟输出 info move
-            move_stats = get_top_moves(go,policy_dir,value_dir,topk = 5)  # 返回多个 move 的 dict
+            move_stats = get_top_moves(go,policy_model,value_model,topk = 5)  # 返回多个 move 的 dict
             for stat in move_stats:
                 print("info", end=' ')
                 for key, value in stat.items():
